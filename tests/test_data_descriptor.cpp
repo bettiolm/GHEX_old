c9e5c6bcdf33b111e9fc911563f7b63d8c07bd21
bettiolm <bettiolm@cscs.ch>
2019-04-18 16:46:06 +0200

    test_data_descriptor passed

--- tests/test_data_descriptor.cpp
+++ tests/test_data_descriptor.cpp
@@ -8,42 +8,16 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-#include <mpi.h>
-#include <fstream>
-#include <sstream>
 #include <iostream>
-#include <list>
-#include <type_traits>
-#include <utility>
-#include <array>
-#include <vector>
-#include <thread>
-#include <algorithm>
-#include <prototype/generic_interfaces.hpp>
+#include <gridtools/meta/utility.hpp>
 #include <prototype/regular_grid_descriptors.hpp>
 #include <prototype/triplet.hpp>
-
-namespace gt = gridtools;
 
 
 namespace test_data_descriptor {
 
 
-    template <int N, typename T>
-    void show_data(T x, std::ostream& s = std::cout) {
-        s << "Data:\n";
-        for (int i = 0; i<N; ++i) {
-            for (int j = 0; j<N; ++j) {
-                s << x[i][j] << ", "; // TO_DO: change access operator
-            }
-            s << "\n";
-        }
-        s << "\n";
-        s.flush();
-    }
-
-
-    template<int rank, typename DA> // type of data array is now templated (DA), as well as rank
+    template<int rank, typename DA, typename DT>
     class data_descriptor_t {
 
       private:
@@ -66,12 +40,12 @@ namespace test_data_descriptor {
         };
 
         std::array<int, rank> m_sizes;
-        DA m_values;
+        DA m_data;
 
       public:
 
         template <typename ...Sizes>
-        data_descriptor_t(const DA& values, Sizes... s) : m_sizes{s...}, m_values{values} {}
+        data_descriptor_t(const DA& data, Sizes... s) : m_sizes{s...}, m_data{data} {}
 
         template <int I>
         int begin() const {
@@ -88,9 +62,24 @@ namespace test_data_descriptor {
             return range(begin<I>(), end<I>());
         }
 
-        template <typename DT, typename ...Coords> // data type is templated
-        DT get_value(Coords... cs) const {
-            return m_values(cs...);
+        template <int size, int... Dims>
+        DT get_data(std::array<int, size> indices, gridtools::meta::integer_sequence<int, Dims...>) const {
+            return m_data(indices[Dims]...);
+        }
+
+        template <int size>
+        DT get_data(std::array<int, size> indices) {
+            return get_data<size>(indices, gridtools::meta::make_integer_sequence<int, size>{});
+        }
+
+        template <int size, int... Dims>
+        void set_data(DT value, std::array<int, size> indices, gridtools::meta::integer_sequence<int, Dims...>) {
+            m_data(indices[Dims]...) = value;
+        }
+
+        template <int size>
+        void set_data(DT value, std::array<int, size> indices) {
+            set_data<size>(value, indices, gridtools::meta::make_integer_sequence<int, size>{});
         }
 
     };
@@ -131,12 +120,25 @@ int main(int argc, char** argv) {
         }
     }
 
+    /* debug and test */
     std::cout << "A \n";
     printbuff(std::cout, a, DIM1 + H1m + H1p, DIM2 + H2m + H2p, DIM3 + H3m + H3p);
 
-    test_data_descriptor::data_descriptor_t<3, array<triple_t<USE_DOUBLE, T1>, layoutmap>> data{a, (DIM1 + H1m + H1p), (DIM2 + H2m + H2p), (DIM3 + H3m + H3p)};
+    test_data_descriptor::data_descriptor_t<3, array<triple_t<USE_DOUBLE, T1>, layoutmap>, triple_t<USE_DOUBLE, T1>> data{
+        a, (DIM1 + H1m + H1p), (DIM2 + H2m + H2p), (DIM3 + H3m + H3p)
+    };
 
-    auto data_1_1_1 = data.get_value<triple_t<USE_DOUBLE, T1>>(1, 1, 1);
+    /* debug and test */
+    std::array<int, 3> indices{1, 1, 1};
+    auto data_1_1_1 = data.get_data<3>(indices);
+    std::cout << "Getter:" << std::endl;
     std::cout << "A(1, 1, 1) = (" << data_1_1_1.x() << ", " << data_1_1_1.y() << ", " << data_1_1_1.z() << ")" << std::endl;
+    triple_t<USE_DOUBLE, T1> value{}; // default constructor: (-1, -1, -1)
+    data.set_data<3>(value, indices);
+    data_1_1_1 = data.get_data<3>(indices);
+    std::cout << "Setter:" << std::endl;
+    std::cout << "A(1, 1, 1) = (" << data_1_1_1.x() << ", " << data_1_1_1.y() << ", " << data_1_1_1.z() << ")" << std::endl;
+
+    return 0;
 
 }
